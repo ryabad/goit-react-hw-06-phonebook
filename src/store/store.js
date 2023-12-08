7f4eb9e022446c75bb9ed23cc0addc5ea0613adc
ryabad <ryabaDa@gmail.com>
2023-12-08 21:36:10 +0200

    update serializableCheck

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { reducer } from './reducer';
 
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
@@ -17,7 +26,9 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
 
